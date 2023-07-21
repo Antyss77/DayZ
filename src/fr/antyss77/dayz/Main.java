e6b5454401914b18a953fc0a5a0f0a3f37615d75
Antyss77 <47704223+Antyss77@users.noreply.github.com>
2023-07-21 16:06:04 +0200

    refactor: Suppression des imports inutilisés dans certaines classes

--- src/fr/antyss77/dayz/Main.java
+++ src/fr/antyss77/dayz/Main.java
@@ -6,11 +6,7 @@ public class Main {
 
 
             // VIDE POUR LE MOMENT
-            // Créer une méthode loadWeapon pour afficher l'arme
 
-            // Ajouter :
-            // Equipement pour les boucliers/pare balles/parachute
-            // Projecticles pour les grandes, cocktail molotov, flashbang,...
 
 
         }
--- src/fr/antyss77/dayz/item/projectiles/grenade/Molotov.java
+++ src/fr/antyss77/dayz/item/projectiles/grenade/Molotov.java
@@ -1,6 +1,5 @@
 package fr.antyss77.dayz.item.projectiles.grenade;
 
-import fr.antyss77.dayz.item.knive.MeleeWeapon;
 import fr.antyss77.dayz.item.projectiles.Projectiles;
 
 public class Molotov extends Projectiles {
