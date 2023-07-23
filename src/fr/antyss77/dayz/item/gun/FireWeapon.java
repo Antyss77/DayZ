7cbdd0312d87754c9c6c9d335b94ff18b9dcd33b
Antyss77 <47704223+Antyss77@users.noreply.github.com>
2023-07-23 18:46:41 +0200

    Nouveau message de commit

--- src/fr/antyss77/dayz/item/gun/FireWeapon.java
+++ src/fr/antyss77/dayz/item/gun/FireWeapon.java
@@ -3,7 +3,6 @@ package fr.antyss77.dayz.item.gun;
 import fr.antyss77.dayz.item.Item;
 
 public class FireWeapon extends Item {
-
     public FireWeapon(String name, ItemCategory category,  ItemType type, int damage) {
         super(name, ItemCategory.FIREARM, type, damage);
     }
---
+++ src/fr/antyss77/dayz/item/gun/assaultrifle/magazine/AssaultRifle.java
@@ -0,0 +1,5 @@
+package fr.antyss77.dayz.item.gun.assaultrifle.magazine;
+
+public class AssaultRifle {
+    // Ici on gère le chargeurs de la catégorie Fusil d'assault
+}
